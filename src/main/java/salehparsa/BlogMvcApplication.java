6372a4aa456017ef4ab28230a253ba39bed9198b
Saleh <sparsa@atlassian.com>
2016-09-02 00:21:21 +0800

    initial commit

---
+++ src/main/java/salehparsa/BlogMvcApplication.java
@@ -0,0 +1,14 @@
+/**
+ * Created by saleh on 9/2/16.
+ */
+package salehparsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMvcApplication.class, args);
+    }
+}
